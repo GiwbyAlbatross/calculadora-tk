c8cf62aa5d4adb97a735c0dd8269f826e8057ed9
Giwby Albatross <giwby.albatross@gmail.com>
2025-03-14 11:55:51 +1100

    Update main.py to enable keybinds

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ import tkinter as tk
 # Módulo próprio
 from app.calculadora import Calculadora
 
+DO_KEYBINDS = True
+
 if __name__ == '__main__':
     master = tk.Tk()
     main = Calculadora(master)
+    if DO_KEYBINDS: main._bind_keyboard()
     main.start()
